09ffb014d6bc5ac3fb8caa5b9ad7b248cf8c59d7
Prabhash Rathore <prabhashrathore@gmail.com>
2015-12-28 09:03:56 -0800

    Add two numbers

--- shellScripts/HelloShell.sh
+++ shellScripts/HelloShell.sh
@@ -4,4 +4,13 @@
 
 echo "What is you name?"
 read name
-echo "Hello, $name" 
+echo "Hello, $name"
+
+# Add two numbers
+NUM_1=23
+NUM_2=45
+NUM_3=$NUM_1+$NUM_2
+
+readonly NUM_3 # Mark this variable as read only
+
+echo "Sum is: $NUM_3"
